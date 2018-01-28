ab9c4493b704bdd531fc0a2d003b0981a31e2cbf
trailblazr <trailblazr@hackerspace-bremen.de>
2018-01-28 23:13:24 +0100

    removed bug with several other rss feed sources improved templates added version added user-agent to not be doored by prominent sites

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # rssfeed-2-html
-Fetches feed contents and generates a nice static website from the contents
+Python application which fetches rss-feed and generates a nice static website with high readability from the available feed entries.
 
 ## Installation (Local)
 
-Create a `virtualenv` before you start installing. There do exist lots of tutorials how to work with **virtualenv**
+Create a `virtualenv` before you start installing. There do exist lots of tutorials on how to work with **virtualenv**.
 
-Then run `pip install -r requirements.txt` to install all needed modules.
+Then run `pip install -r requirements.txt` to install all needed modules. There are some modules related to crypto which are needed to access TLS secured sites.
 
 ## Usage
 
@@ -28,6 +28,15 @@ optional arguments:
 e.g: python app.py -f https://www.netzpolitik.org/feed -o index.html -r YES
 ```
 
+There are **three arguments** you can provide to the commandline call:
+
+* `-f` or `--feedurl` which expects the http/s-url to the rss-feed  
+**Example:** `python app.py -f http://www.tagesschau.de/xml/rss2`
+* `-o` or `--output` which expects an absolute or relative path to a filename where to store the static website  
+**Example:** `python app.py -o /home/www/mywebsite/htdocs/index.html` 
+* `-r` or `--readable` expects a `YES` or a `NO` (casesensitive) to render site with readability support or without  
+**Example:** `python app.py -r YES` will render a page with readability support 
+
 ## Example
 
 Type `python app.py -f https://www.hackerspace-bremen.de/feed -o space.html -r NO`
@@ -41,5 +50,9 @@ You can easily set this app up to do some fancy work on a server, e.g. by engagi
 * See `crontab.txt` to have an idea/example of how the crontab entry could look like. Btw you edit the crontab entries on your server usually by entering `crontab -e` which will edit the important file with `vi` then you need to add the lines from `crontab.txt` and change the serverdomain to activate it.
 * The entry seen in the example of `crontab.txt` will trigger a script `cron.sh` which should be made executable by the cron daemon. So in doubt apply a `chmod ug+rx cron.sh` to make it available to cron.
 * Test all these configured scripts **before adding the cron entry** by just entering `./cron.sh` on the commandline and look what happens.
+* For **readability** to work, you need to copy the following folders into your site directory
+  * `css`
+  * `js`
+  * `images`
 
 Be careful with the paths to files and check the permissions and ownership and groupmembership of files if something fails. Often html-files need the ownership or group membership of `www-data` e.g. You could also do a `tail -f /var/log/syslog` to debug if your cronjob posts errors there. In some cases cron also sends local mails via postfix. You could check mails bei entering `mail` on your UNIX box.
--- app.py
+++ app.py
@@ -22,10 +22,14 @@ import feedparser
 import BeautifulSoup
 import bs4 as bs
 from readability import Document
+from datetime import datetime, date
 
 from feedparser import parse as parse_feed
 from mako.template import Template
 
+APP_BRANDNAME       = 'FeedReadability'
+APP_BUILD           = 10
+APP_RELEASE         = '1.0'
 APP_PATH            = os.path.dirname(os.path.abspath(__file__))
 APP_DEBUG           = False
 
@@ -38,7 +42,9 @@ def make_site_with_rssfeed_readable_again(url, filename, is_clean):
     """Convert feed to an HTML."""
     with open(filename, 'w') as file_object:
         print "\nOPENING URL: "+url+"\n\n"
-        response = requests.get( url )
+        headers = {'User-Agent': APP_BRANDNAME+'/'+APP_RELEASE+' (Unix; Intel OS Nine 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36' }
+
+        response = requests.get( url, headers=headers )
         mystr = response.text
 
         # remove heigh and width in images because CSS will do that
@@ -51,7 +57,7 @@ def make_site_with_rssfeed_readable_again(url, filename, is_clean):
         mystr = mystr.replace(u"Mit Deiner finanziellen Hilfe unterstützt Du unabhängigen Journalismus.", "")
         
         if APP_DEBUG:
-            print "FEED:\n"+mystr+"\n****************************"
+            print "FEED:\n"+ str( mystr )+"\n****************************"
 
         feedtitle = None
         try:
@@ -63,6 +69,8 @@ def make_site_with_rssfeed_readable_again(url, filename, is_clean):
         except Exception, e:
             print "PARSING-ERROR: " + str( e )
             print( traceback.format_exc() )
+            pass
+
 
         if not feedtitle:
             feedtitle = DEFAULT_TITLE
@@ -72,7 +80,10 @@ def make_site_with_rssfeed_readable_again(url, filename, is_clean):
         else:
             template = APP_PATH+'/'+'template_readable.html'
 
-        html_content = Template(filename=template, output_encoding='utf-8').render(feedurl=url,entries=entries,feedtitle=feedtitle)
+        if APP_DEBUG:
+            print "\n ENTRIES TO RENDER: "+ str( len(entries) ) +"\n"
+        html_footer = site_footer_html()
+        html_content = Template(filename=template, output_encoding='utf-8').render(feedurl=url,entries=entries,feedtitle=feedtitle,footer=html_footer)
         
         if APP_DEBUG:
             print "HTML:\n"+ html_content+"\n****************************"
@@ -83,6 +94,17 @@ def make_site_with_rssfeed_readable_again(url, filename, is_clean):
 
         else:
             file_object.write( html_content )
+
+# footer
+def site_footer_html():
+    STYLE_HR = 'border: 0;height: 0;border-top: 1px solid rgba(0, 0, 0, 0.1);'
+    today = datetime.now()
+    version_info = 'Release '+str(APP_RELEASE)+' (build '+str(APP_BUILD)+') of '+APP_BRANDNAME
+    footer = '<hr style="margin-top:50px;" class="style3" /><div style="margin-top:10px;margin-bottom:10px;">'
+    footer = footer + '<div style="text-align:center;color:gray;"><small>'
+    footer = footer + 'created 2018-'+str( today.year )+', by trailblazr &middot; '+version_info
+    footer = footer + '</small></div>'
+    return footer
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Fetch an RSS-Feed and generate a Webpage from it.', 
