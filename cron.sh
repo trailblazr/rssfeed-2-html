dc48494b0eba828a629826bb2022f58124bdce42
trailblazr <trailblazr@hackerspace-bremen.de>
2018-01-28 14:25:22 +0100

    make files consistent

--- cron.sh
+++ cron.sh
@@ -2,4 +2,4 @@
 # Call this file from a crontab entry on any server
 # it calls itself a shell script to launch the python application
 
-sh /home/www/config.seewetterpro.de/webapp/update_feed.sh
+sh /home/www/yourserverdomain/webapp/update_feed.sh
